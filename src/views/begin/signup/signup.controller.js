8a4a8cc2d5775a82366c3110c738d33a566cfbea
Manideep <manideep@codemymobile.com>
2017-07-27 12:22:36 +0530

    signup css improvements

--- src/views/begin/signup/signup.controller.js
+++ src/views/begin/signup/signup.controller.js
@@ -6,24 +6,39 @@
 
   function signupController($state, $scope, $http, exploreService, LANGUAGE, loaderService, languagereadService, CATEGORY, expertiseNameService) {
     var vm = this;
-    vm.formisValid ="";
+    vm.formisValid = "";
     vm.formStep = 1;
     vm.formValid = false;
+    vm.checkboxChecked = true;
+    vm.password = '';
+    vm.cnfmpassword = '';
+    vm.comparepassword = false;
     vm.tourData = {};
-    vm.register = function(){
-    console.log("from valid status", vm.formValid);
-      // if(vm.formValid){
-          vm.formisValid ="";
+    vm.formValid_sec = false
+    vm.register = function () {
+      console.log("from valid status", vm.formValid);
+      if (vm.formValid) {
+        vm.formisValid = "";
+        console.log(vm.password, vm.cnfmpassword);
+        if (vm.password !== vm.cnfmpassword) {
+          vm.comparepassword = true;
+        }
+        if (vm.terms) {
+          vm.checkboxChecked = false;
           vm.formStep = 2;
+        }
+      } else {
+        vm.formisValid = "Please fill the form again properly";
+      }
+    };
+    vm.updatebasic= function(){
+      if(vm.formValid_sec){
+        vm.formisValiderror = '';
+      }else{
+        vm.formisValiderror = 'Please fill the form again. ';
+      }
 
-      // }else{
-          // vm.formisValid ="Please fill the form again properly";
-      // }
-
-    }
-
-
-
+    };
   }
   module.controller('signupController', signupController);
 })(angular.module('Citygyd.Controllers'));
